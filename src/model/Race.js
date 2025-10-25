11ec26e5fe9da946a860e0e9c1bd468159bdf4bc
jsomnium <jsomnium@naver.com>
2025-10-25 19:10:11 +0900

    chore(race): canMove 함수 하단 이동
    - canMove 함수 하단 이동하여 코드 가독성 향상

--- src/model/Race.js
+++ src/model/Race.js
@@ -3,10 +3,6 @@ import { Random } from "@woowacourse/mission-utils";
 class Race {
   #cars;
   #rounds;
-  #canMove() {
-    const randomNumber = Random.pickNumberInRange(0, 9);
-    return randomNumber >= 4;
-  }
 
   constructor(cars, rounds) { 
     this.#cars = cars;
@@ -17,16 +13,21 @@ class Race {
     const playRounds = this.#rounds;
 
     for (let round = 0; round < playRounds; round++) {
-      this.#cars.forEach(car => {
-        if (this.#canMove()) {
-          car.move();
-        }
-      });
+      this.#playRounds();
     }
   }
 
-    playRounds() {
-      
+  #canMove() {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+    return randomNumber >= 4;
+  }
+
+  #playRounds() {
+    this.#cars.forEach(car => {
+      if (this.#canMove()) {
+        car.move();
+      }
+    });
   }
 }
 
